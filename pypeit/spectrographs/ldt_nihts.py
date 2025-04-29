93a07c332686a6b25745d6aa2e0de3e9bcea9c55
Timothy Ellsworth-Bowers <tbowers@lowell.edu>
2025-04-28 17:06:30 -0700

    Conformal change in slit parsing
    Change made in base code; conformal change here.
    
    	modified:   pypeit/spectrographs/ldt_nihts.py

--- pypeit/spectrographs/ldt_nihts.py
+++ pypeit/spectrographs/ldt_nihts.py
@@ -320,11 +320,9 @@ class LDTNIHTSSpectrograph(spectrograph.Spectrograph):
         par["calibrations"]["slitedges"]["max_nudge"] = 5  # Default: None
         par["calibrations"]["slitedges"]["minimum_slit_gap"] = 0  # Default: None
         par["calibrations"]["slitedges"]["minimum_slit_length"] = 10.0  # Default: None
-        par["calibrations"]["slitedges"]["rm_slits"] = [
-            "1:512:900",
-            "1:512:950",
-            "1:512:1000",
-        ]  # Remove any spurious slits at +spatial range
+        par["calibrations"]["slitedges"][
+            "rm_slits"
+        ] = "1:512:900; 1:512:950; 1:512:1000"  # Remove any spurious slits at +spatial range
         par["calibrations"]["slitedges"]["smash_range"] = [0.3, 0.9]  # Default: None
         par["calibrations"]["slitedges"]["sync_predict"] = "nearest"  # Default: 'pca'
         par["calibrations"]["slitedges"]["trace_median_frac"] = 0.1  # Default: None
