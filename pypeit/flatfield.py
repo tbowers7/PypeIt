fb52090e065959293116dfbd011e82472bac1ac1
Timothy Ellsworth-Bowers <tbowers@lowell.edu>
2025-04-28 17:07:04 -0700

    Allow Slits file to be passed to ``pypeit_chk_flats``
    In the event when the flat field and slits calibration files do not
    share the same ``calib_key`` (e.g., "A_0_DET01", "B_all_DET02",
    etc.), allow the desired Slits file to be specified in the call to
    the ``pypeit_chk_flats`` script.
    
    	modified:   pypeit/flatfield.py
    	modified:   pypeit/scripts/chk_flats.py

--- pypeit/flatfield.py
+++ pypeit/flatfield.py
@@ -424,7 +424,7 @@ class FlatImages(calibframe.CalibFrame):
         pixelflat_finecorr, illumflat_finecorr = None, None
         pixelflat_totalillum, illumflat_totalillum = None, None
 
-        if slits is None and self.calib_dir is not None or self.calib_key is not None:
+        if slits is None and (self.calib_dir is not None or self.calib_key is not None):
             # If the slits are not defined, and the relevant attributes are set,
             # try to read the associated SlitTraceSet
             slits_file = slittrace.SlitTraceSet.construct_file_name(self.calib_key,
--- pypeit/scripts/chk_flats.py
+++ pypeit/scripts/chk_flats.py
@@ -16,6 +16,10 @@ class ChkFlats(scriptbase.ScriptBase):
                                     width=width)
         parser.add_argument('file', type=str,
                             help='PypeIt Flat file [e.g. Flat_A_1_DET01.fits]')
+        parser.add_argument('--slits_file', type=str, default=None,
+                            help='PypeIt Slits file [e.g. Slits_A_1_01.fits]. If this file does '
+                                 'not exist or is not provided, PypeIt will attempt to read the '
+                                 'default file name (in the Calibrations directory).')
         parser.add_argument("--type", type=str, default='all',
                             help="Which flats to display. Must be one of: pixel, illum, all")
         parser.add_argument('--try_old', default=False, action='store_true',
@@ -26,12 +30,12 @@ class ChkFlats(scriptbase.ScriptBase):
     def main(args):
 
         from pypeit import flatfield
+        from pypeit import slittrace
 
         chk_version = not args.try_old
 
         # Load
         flatImages = flatfield.FlatImages.from_file(args.file, chk_version=chk_version)
+        slits = slittrace.SlitTraceSet.from_file(args.slits_file, chk_version=chk_version) if args.slits_file is not None else None
         # Show
-        flatImages.show(args.type, chk_version=chk_version)
-
-
+        flatImages.show(args.type, slits=slits, chk_version=chk_version)
